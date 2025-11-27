d7c610b15bd93a93f69c1fddcdf464316651f1c1
go76loj <go76loj@mytum.de>
2025-11-27 19:02:20 +0100

    Funktion: generate_AES_key(PBKDF2HMAC derive funktion)

--- test.py
+++ test.py
@@ -1,3 +1,32 @@
-from z3 import * 
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.backends import default_backend
+import os
+import base64
 
-print("hallo world")
+def generate_AES_key(passwort: str, salt:bytes)->bytes:
+    KDF = PBKDF2HMAC(
+        algorithm = hashes.SHA256(),
+        length = 32, 
+        salt = salt,
+        iterations = 200_000,
+        backend=default_backend())
+
+    key = KDF.derive(passwort.encode())
+    return key
+
+
+if __name__ == "__main__":
+    salt = os.urandom(16)
+    pw = "meinPasswort123"
+
+    key1 = generate_AES_key(pw, salt)
+    key2 = generate_AES_key(pw, salt)
+
+    print("Gleicher Key?", key1 == key2)
+
+    salt2 = os.urandom(16)
+    key3 = generate_AES_key(pw, salt2)
+
+    print("Andere Salts → anderer Key?", key1 != key3)
+
